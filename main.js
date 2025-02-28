27459ab4ebd1fe29f7e47de4d3833477eccd6765
TeRaNozaVr1 <117114468+TeRaNozaVr1@users.noreply.github.com>
2025-02-28 16:37:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { Connection, PublicKey, Transaction, SystemProgram } from "@solana/web3.js";
+const connection = new Connection("https://api.mainnet-beta.solana.com");
+
 import { Buffer } from "buffer"; // Додаємо підтримку buffer
 
 window.Buffer = Buffer; // Робимо buffer доступним у браузері
